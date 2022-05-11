10f8c0c017ff1bea79eee71896563cc3e3f48667
Ambaliya Dhruv <ambaliyadhruv1112@gmail.com>
2022-05-11 10:37:29 +0530

    add filter in population query

--- src/controller/route.controller.js
+++ src/controller/route.controller.js
@@ -1,4 +1,5 @@
 
+const { query } = require("express");
 const express=require("express");
 
 const router=express.Router();
@@ -7,10 +8,20 @@ const Routeof=require("../model/route.js");
 
 router.get("/",async(req,res)=>{
     try {
-        //  const route=await Routeof.find().populate({path:"startAirport",match:{airport_name:{$eq:"Indra Gandhi Internation Airport"}},select:"airport_name"}).populate({path:"endingAirport",match:{airport_name:{$eq:"Sardar Patel Internation Airport"}},select:"airport_name"}).lean().exec();
-
+  
+         let start=req.query.start;
+         let end =req.query.end;
+        
          const route=await Routeof.find().populate("startAirport").populate("endingAirport").lean().exec();
-         return res.status(200).send(route);
+         let data;
+         if(start && end){
+             data=route.filter(x=>{if(x.startAirport.city===start && x.endingAirport.city===end){
+                return x;
+           }});
+         }else{
+            return res.status(404).send(route);
+         }
+         return res.status(200).send(data);
     } catch (error) {
         return res.status(400).send({message:error.message});
     }
